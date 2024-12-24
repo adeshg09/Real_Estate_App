e0bb2e6b2462180fa96d1661ea86067bb70cd69d
adesh09 <adeshgadage0908@gmail.com>
2024-12-25 02:53:17 +0530

    updated code of v1

---
+++ socket/app.js
@@ -0,0 +1,44 @@
+import { Server } from "socket.io";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const io = new Server({
+  cors: {
+    origin: process.env.CLIENT_URL,
+  },
+});
+
+let onlineUser = [];
+
+const addUser = (userId, socketId) => {
+  const userExits = onlineUser.find((user) => user.userId === userId);
+  if (!userExits) {
+    onlineUser.push({ userId, socketId });
+  }
+};
+
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+  });
+
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    io.to(receiver.socketId).emit("getMessage", data);
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+  });
+});
+
+io.listen(process.env.PORT || 4000);
